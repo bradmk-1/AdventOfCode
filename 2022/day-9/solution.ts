735f17ae432fa6f4a3c4c9e9b6d50c5622e00c18
Brad <bradleyjm2001@gmail.com>
2022-12-10 21:07:33 +0000

    day 10 solutions

--- 2022/day-9/solution.ts
+++ 2022/day-9/solution.ts
@@ -3,8 +3,8 @@ const data = fs.readFileSync('./input.txt', 'utf-8').split('\r\n');
 
 //Part One
 
-const partOne = (input: string[]) => {
-    const visitedLocations = new Set([0, 0]);
+const partOne = (input: string[]): void => {
+    const visitedLocations = new Set();
 
     //X, Y
     const head = [0, 0];
@@ -26,12 +26,46 @@ const partOne = (input: string[]) => {
             head[0] -= Number(move.split(' ')[1]);
         }
 
-        //Work out correct tail path here
+        //Calc tail coords based on head coords
+
+        //Up or right moves
+        if (Math.abs(head[0] - tail[0]) > 1) {
+            for (let i = 0; i <= head[0]; i++) {
+                if (head[0] > tail[0]) {
+                    tail[0] += 1;
+                } else {
+                    tail[0] -= 1
+                }
+                visitedLocations.add(tail.toString());
+            };
+        }
+
+        if (Math.abs(head[1] - tail[1]) > 1) {
+            for (let i = 0; i <= head[1]; i++) {
+                if (head[1] > tail[1]) {
+                    tail[1] += 1;
+                } else {
+                    tail[1] -= 1
+                }
+                visitedLocations.add(tail.toString());
+            };
+        };
+        console.log(tail);
 
     };
-    console.log(visitedLocations.size);
+    console.log(visitedLocations.size + 1);
 };
 
 partOne(data);
+
+
+//THIS IS FINE
+// [1, 3]
+// [2, 4]
+
+//THIS ISNT FINE
+// [1, 2]
+// [2, 4]
+
 
 //Goal -> find how many positions the tail vists at least once
