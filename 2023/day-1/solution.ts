b7e292f66e5e50692a7d6f3160fda7bc895fc79f
Brad Moore <bradleyjm2001@gmail.com>
2023-12-01 15:19:25 +0000

    2023 - day 1 - part 1

---
+++ 2023/day-1/solution.ts
@@ -0,0 +1,18 @@
+import fs from 'node:fs';
+const data = fs.readFileSync('input.txt', 'utf-8').split('\r\n')
+
+const partOne = (input: string[]): void => {
+    const output: number[] = [];
+
+    for (const parsedInput of input) {
+        const splitInput = parsedInput.split('');
+        const firstDigit = splitInput.find((el) => el.match(/^[^\d]*(\d+)/))
+        const secondDigit = splitInput.reverse().find((el) => el.match(/^[^\d]*(\d+)/))
+        output.push(Number(`${firstDigit}${secondDigit}`))
+    }
+
+    console.log(output.reduce((acc: number, curr: number) => acc + curr));
+};
+
+partOne(data);
+
